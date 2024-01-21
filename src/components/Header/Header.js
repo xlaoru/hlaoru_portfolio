602fb9a65afc31facf5fb92523c0ad4906cdd5f6
xlaoru <sia212007@gmail.com>
2024-01-21 21:48:00 +0200

    v1.5

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,16 +12,16 @@ import ListItemText from '@mui/material/ListItemText';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
-import Avatar from '@mui/material/Avatar'
+import Avatar from '@mui/material/Avatar';
+import CloseIcon from '@mui/icons-material/Close';
 
-import hlaoruLogo from '../../media/hlaoru_logo.jpg'
+import hlaoruLogo from '../../media/hlaoru_logo.jpg';
 
-const drawerWidth = 240;
 const navItems = [
-  {title: 'Home', link: '#home'},
-  {title: 'About', link: '#about'},
-  {title: 'Projects', link: '#projects'},
-  {title: 'Contact', link: '#contact'},
+  { title: 'Home', link: '#home' },
+  { title: 'About', link: '#about' },
+  { title: 'Projects', link: '#projects' },
+  { title: 'Contact', link: '#contact' },
 ];
 
 function Header(props) {
@@ -32,28 +32,39 @@ function Header(props) {
     setMobileOpen((prevState) => !prevState);
   };
 
+  const handleMenuItemClick = () => {
+    setMobileOpen(false);
+  };
+
   const drawer = (
-    <Box onClick={handleDrawerToggle}>
-      <Divider />
-      <List sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-        {navItems.map((item, index) => (
-          <ListItem key={index} disablePadding sx={{p: 0, m: 0}}>
-            <ListItemButton sx={{display: 'flex', justifyContent: 'center', pl: 0}}>
-              <a style={{'color': '#1e1e1e', 'textDecoration': 'none'}} href={item.link}><ListItemText sx={{color: '#1e1e1e'}} /><h3>{item.title}</h3></a>
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+    <Box style={{ height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Box sx={{ borderBottom: '1px solid #ccc', width: '100%', textAlign: 'right', pr: 1 }}>
+        <IconButton color="inherit" onClick={handleDrawerToggle}>
+          <CloseIcon />
+        </IconButton>
+      </Box>
+      <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+        <List>
+          {navItems.map((item, index) => (
+            <ListItem key={index} disablePadding sx={{ p: 1.5, m: 0, fontSize: 25 }}>
+              <ListItemButton sx={{ display: 'flex', justifyContent: 'center', pl: 0 }} onClick={handleMenuItemClick}>
+                <a style={{ 'color': '#1e1e1e', 'textDecoration': 'none' }} href={item.link}><ListItemText sx={{ color: '#1e1e1e' }} /><h3>{item.title}</h3></a>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
     </Box>
   );
+  
 
   const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex'}} >
+    <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <AppBar component="nav">
-        <Toolbar sx={{display: 'flex', justifyContent: 'space-between'}} style={{'minHeight': '50px'}}>
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }} style={{ 'minHeight': '50px' }}>
           <IconButton
             color="inherit"
             aria-label="open drawer"
@@ -62,15 +73,15 @@ function Header(props) {
             sx={{ mr: 2, display: { sm: 'none' } }}
           >
             <MenuIcon />
-            </IconButton>
-            <Avatar alt="hlaoru_logo" src={hlaoruLogo} sx={{boxShadow: '0px 0px 5px 0px rgba(30,30,30,1)'}} />
-            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-                {navItems.map((item, index) => (
-                <Button key={index} sx={{color: '#1e1e1e', fontWeight: 'bold'}}>
-                  <a style={{'color': '#1e1e1e', 'textDecoration': 'none'}} href={item.link}>{item.title}</a>
-                </Button>
-                ))}
-            </Box>
+          </IconButton>
+          <Avatar alt="hlaoru_logo" src={hlaoruLogo} sx={{ boxShadow: '0px 0px 5px 0px rgba(30,30,30,1)' }} />
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item, index) => (
+              <Button key={index} sx={{ color: '#1e1e1e', fontWeight: 'bold' }}>
+                <a style={{ 'color': '#1e1e1e', 'textDecoration': 'none' }} href={item.link} onClick={handleMenuItemClick}>{item.title}</a>
+              </Button>
+            ))}
+          </Box>
         </Toolbar>
       </AppBar>
       <Box component="nav">
@@ -83,8 +94,15 @@ function Header(props) {
             keepMounted: true,
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth, height: 'auto', borderRadius: '0 15px 15px 15px' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: '100%',
+              height: '100%',
+              top: 0,
+              position: 'fixed',
+              zIndex: 999,
+              borderRadius: '0 15px 15px 15px',
+            },
           }}
         >
           {drawer}
@@ -94,4 +112,4 @@ function Header(props) {
   );
 }
 
-export default Header
+export default Header;
